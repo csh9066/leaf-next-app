aeb43d2177fc317b5a63c569f15bfc8737cf9d85
seungho <50465901+csh9066@users.noreply.github.com>
2022-02-28 20:03:50 +0900

    Refactory Header

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -12,63 +12,47 @@ interface Props {
   type: "default" | "navigator";
 }
 
-function Default() {
+function Header({ type }: Props): ReactElement {
+  const router = useRouter();
   const isLoggedIn = useIsLoggedIn();
   const { onOpen } = useLoginModal();
-  return (
-    <>
-      <Link href="/">
-        <a className="flex justify-center items-center">
-          <Logo />
-        </a>
-      </Link>
-      {isLoggedIn ? (
-        <Link href="/cart">
-          <a className="flex justify-center items-center">
-            <ShoppingBag />
-            {/* <div className="flex justify-center items-center rounded-xl w-5 h-5 bg-primary text-white absolute top-1 right-3">
-              3
-            </div> */}
-          </a>
-        </Link>
-      ) : (
-        <a className="flex justify-center items-center" onClick={onOpen}>
-          <ShoppingBag />
-        </a>
-      )}
-    </>
-  );
-}
 
-function Navigator() {
-  const rotuer = useRouter();
-  return (
-    <>
-      <>
-        <a
-          className="flex justify-center items-center cursor-pointer"
-          onClick={rotuer.back}
-        >
-          <ArrowLeft />
-        </a>
-        <Link href="/cart">
-          <a className="flex justify-center items-center">
-            <ShoppingBag />
-          </a>
-        </Link>
-      </>
-    </>
-  );
-}
-
-function Header({ type }: Props): ReactElement {
   return (
     <Container
       className="flex justify-between h-12 px-5 fixed bg-white z-10"
       el="header"
     >
-      {type === "default" && <Default />}
-      {type === "navigator" && <Navigator />}
+      <>
+        {type === "default" ? (
+          <Link href="/">
+            <a className="flex justify-center items-center">
+              <Logo />
+            </a>
+          </Link>
+        ) : (
+          <a
+            className="flex justify-center items-center cursor-pointer"
+            onClick={router.back}
+          >
+            <ArrowLeft />
+          </a>
+        )}
+
+        {isLoggedIn ? (
+          <Link href="/cart">
+            <a className="flex justify-center items-center">
+              <ShoppingBag />
+              {/* <div className="flex justify-center items-center rounded-xl w-5 h-5 bg-primary text-white absolute top-1 right-3">
+              3
+            </div> */}
+            </a>
+          </Link>
+        ) : (
+          <a className="flex justify-center items-center" onClick={onOpen}>
+            <ShoppingBag />
+          </a>
+        )}
+      </>
     </Container>
   );
 }
